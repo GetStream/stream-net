b075caefd7e555d221b5d6cfdc7ed47816037605
shawnspeak <shawnb@xcarab.com>
2015-02-15 18:54:50 -0800

    Missing file

---
+++ src/stream-net/Follower.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Stream
+{
+    public class Follower
+    {
+        [JsonProperty("feed_id")]
+        public String FeedId { get; set; }
+
+        [JsonProperty("target_id")]
+        public String TargetId { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
