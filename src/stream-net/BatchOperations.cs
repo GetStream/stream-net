d4c5a881e3e993134c0e2fdd77eec78e4877e650
Link512 <horatiu@getstream.io>
2018-08-06 12:16:02 +0200

    [AC-64] fix copy limit

--- src/stream-net/BatchOperations.cs
+++ src/stream-net/BatchOperations.cs
@@ -49,8 +49,8 @@ namespace Stream
 
         public async Task FollowMany(IEnumerable<Follow> follows, int activityCopyLimit = StreamClient.ActivityCopyLimitDefault)
         {
-            if (activityCopyLimit < 1)
-                throw new ArgumentOutOfRangeException("activityCopyLimit", "Activity copy limit must be greater than 0");
+            if (activityCopyLimit < 0)
+                throw new ArgumentOutOfRangeException("activityCopyLimit", "Activity copy limit must be greater than or equal to 0");
             if (activityCopyLimit > StreamClient.ActivityCopyLimitMax)
                 throw new ArgumentOutOfRangeException("activityCopyLimit", string.Format("Activity copy limit must be less than or equal to {0}", StreamClient.ActivityCopyLimitMax));
 
