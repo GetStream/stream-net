92e060ab91b2c5f560b099d550af2b2183efd223
Link512 <horatiu@getstream.io>
2018-10-30 12:17:14 +0100

    add user session token

--- src/stream-net/IStreamClient.cs
+++ src/stream-net/IStreamClient.cs
@@ -3,12 +3,14 @@ using System.Threading.Tasks;
 
 namespace Stream
 {
-	public interface IStreamClient
-	{
-		IBatchOperations Batch { get; }
-		Collections Collections { get; }
+    public interface IStreamClient
+    {
+        IBatchOperations Batch { get; }
+        Collections Collections { get; }
 
-		Task ActivityPartialUpdate(string id = null, ForeignIDTime foreignIDTime = null, GenericData set = null, IEnumerable<string> unset = null);
-		IStreamFeed Feed(string feedSlug, string userId);
-	}
+        Task ActivityPartialUpdate(string id = null, ForeignIDTime foreignIDTime = null, GenericData set = null, IEnumerable<string> unset = null);
+        IStreamFeed Feed(string feedSlug, string userId);
+
+        string SessionToken(string userId, IDictionary<string, object> extraData = null);
+    }
 }
