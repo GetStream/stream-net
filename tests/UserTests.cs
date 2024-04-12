f6f135f9108544bcd66b2fd25539235b0e1fc0f9
Tiago Farto <tiago.farto@getstream.io>
2024-04-12 12:28:17 +0100

    chore: discard accidental file

--- tests/UserTests.cs
+++ tests/UserTests.cs
@@ -1,6 +1,5 @@
 using NUnit.Framework;
 using Stream;
-using Stream.Models;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -71,23 +70,6 @@ namespace StreamNetTests
             {
                 var x = await Client.Users.GetAsync(userId);
             });
-        }
-
-        [Test]
-        public async Task TestForeignIdTimeBug()
-        {
-            // local credentials:
-            const string api_key = "qk4nn7rpcn75";
-            const string api_secret = "vz4yhgcwums5h64uh8xbx58c9yfgtkjneab5ch9skds93xzqd8mv6dc824dusnvt";
-
-            Environment.SetEnvironmentVariable("STREAM_URL", "http://localhost:8000");
-
-            var client = new StreamClient(api_key, api_secret);
-
-            var activityId = "a20abf36-afd1-11ee-836a-ead473e55f11";
-            var result = await client.Reactions.FilterAsync(ReactionFiltering.Default.WithRanking("popular"), ReactionPagination.By.ActivityId(activityId));
-            Assert.NotNull(result);
-
         }
     }
 }
