e006f7f48e1ad97ae74c04c4704f895abfe04db4
Tiago <1585571+xernobyl@users.noreply.github.com>
2024-04-23 02:11:46 -0700

    ci: fix script (#134)

--- scripts/create_release_branch.sh
+++ scripts/create_release_branch.sh
@@ -4,9 +4,8 @@ VERSION=$1
 echo "Preparing release $VERSION"
 
 # Update .csproj file
-# This regex switches all occurences of >0.21.0< to >0.22.0<
-# So <PackageVersion>0.21.0</PackageVersion> becomes <PackageVersion>0.22.0</PackageVersion>
-sed -i -r "s/>[0-9]+\.[0-9]+\.[0-9]+</>$VERSION</g" src/stream-net.csproj
+# This regex to update Version tag in .csproj file
+sed -i -r "s/\(<Version>\).*\(<\/Version>\)/\1$VERSION\2/" src/stream-net.csproj
 
 # Create changelog
 # --skip.commit: We manually commit the changes
@@ -15,7 +14,7 @@ sed -i -r "s/>[0-9]+\.[0-9]+\.[0-9]+</>$VERSION</g" src/stream-net.csproj
 # we set it to an empty string
 npx --yes standard-version@9.3.2 --release-as "$VERSION" --skip.tag --skip.commit --tag-prefix=
 
-git config --global user.name 'github-actions' 
+git config --global user.name 'github-actions'
 git config --global user.email 'release@getstream.io'
 git checkout -q -b "release-$VERSION"
 git commit -am "chore(release): $VERSION"
